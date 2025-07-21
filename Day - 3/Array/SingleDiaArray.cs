33b6c3662da28fb2b0ddfcb670e4616b2ae7bd2c
Kapil Rathod <rathodkapil9657@gmail.com>
2025-07-21 18:54:33 +0530

    First

---
+++ Day - 3/Array/SingleDiaArray.cs
@@ -0,0 +1,17 @@
+using System;
+
+class SingleDiaArray
+{
+    public static void Main(string[] args)
+    {
+        int NoStudents = 5;
+        string[] names = new string[NoStudents];
+
+        Console.WriteLine("Enter the name Of Students :");
+        for (int i = 0; i < NoStudents; i++)
+        {
+            Console.WriteLine("Enter the 1st name fo Student : ");
+        }
+    }
+    
+}
